e7ce781863fe0aedd5923d3216c4e6240eb746f8
manu2729android <53445848+manu2729android@users.noreply.github.com>
2021-02-23 23:04:59 +0530

    Create buslist.php

---
+++ buslist.php
@@ -0,0 +1,17 @@
+<?php
+$json = '[ {
+        "id": 9222968140497289234,
+        "name": "doggie",
+        "photoUrls": [],
+        "tags": [],
+        "status": "available"
+    },
+    {
+        "id": 9222968140497289235,
+        "name": "doggie",
+        "photoUrls": [],
+        "tags": [],
+        "status": "available"
+    }]';
+    return $json ;
+    ?>
